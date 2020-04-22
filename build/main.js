a4bbb49169ce26aa926d5b1f0a0e0cc807871ec1
opina-ai <kayque-willy@hotmail.com>
2020-04-22 00:04:15 -0300

    first commit

--- build/main.js
+++ build/main.js
@@ -704,7 +704,7 @@ var map = {
 		6
 	],
 	"../pages/prioritization/prioritization.module": [
-		295,
+		296,
 		4
 	],
 	"../pages/questionaries-list/questionaries-list.module": [
@@ -712,7 +712,7 @@ var map = {
 		3
 	],
 	"../pages/questionary/questionary.module": [
-		296,
+		295,
 		2
 	],
 	"../pages/respondent-profile/respondent-profile.module": [
@@ -1288,8 +1288,8 @@ var AppModule = /** @class */ (function () {
                         { loadChildren: '../pages/thankyou/thankyou.module#ThankyouPageModule', name: 'ThankyouPage', segment: 'thankyou', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/intro/intro.module#IntroPageModule', name: 'IntroPage', segment: 'intro', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/questionaries-list/questionaries-list.module#QuestionariesListPageModule', name: 'QuestionariesListPage', segment: 'questionaries-list', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/prioritization/prioritization.module#PrioritizationPageModule', name: 'PrioritizationPage', segment: 'prioritization', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/questionary/questionary.module#QuestionaryPageModule', name: 'QuestionaryPage', segment: 'questionary', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/prioritization/prioritization.module#PrioritizationPageModule', name: 'PrioritizationPage', segment: 'prioritization', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/respondent-profile/respondent-profile.module#RespondentProfilePageModule', name: 'RespondentProfilePage', segment: 'respondent-profile', priority: 'low', defaultHistory: [] }
                     ]
                 }),
